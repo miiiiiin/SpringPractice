18875863a80a7ab8635d6702b745ec0b5872382d
songkyung <min.songkyung@gmail.com>
2024-08-21 16:03:06 +0900

    opencsv 활용한 대량 데이터 등록

--- MiniProjects/CalendarAppApplication/src/main/java/com/example/CalendarAppApplication/CalendarAppApplication/CalendarAppApplication.java
+++ MiniProjects/CalendarAppApplication/src/main/java/com/example/CalendarAppApplication/CalendarAppApplication/CalendarAppApplication.java
@@ -3,7 +3,9 @@ package com.example.CalendarAppApplication.CalendarAppApplication;
 import event.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reader.EventCsvReader;
 
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -17,26 +19,22 @@ public class CalendarAppApplication {
 //		SpringApplication.run(CalendarAppApplication.class, args);
 //	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 //		List<AbstractEvent> list = new ArrayList<>();
 		Schedule schedule = new Schedule();
 		HashSet<String> participants = new HashSet<String>();
 		participants.add("sk.min");
 
-		Meeting meeting1 = new Meeting(1, "meeting1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), participants, "meetingRoomA", "study");
-
-		schedule.add(meeting1);
-
-		Todo todo1 = new Todo(2, "todo2", ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4), "할 일 적기2");
-		schedule.add(todo1);
+//		Meeting meeting1 = new Meeting(1, "meeting1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), participants, "meetingRoomA", "study");
+//		schedule.add(meeting1);
+//		Todo todo1 = new Todo(2, "todo2", ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4), "할 일 적기2");
+//		schedule.add(todo1);
 
 		// 예외 발생 사례
 //		Todo todo2 = new Todo(3, "todo3", ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4), "할 일 적기3");
 //		schedule.add(todo2);
-
 //		schedule.forEach(Event::print);
-
-		schedule.printAll();
+//		schedule.printAll();
 
 		// Meeting 클래스인 것만 가져와서 필터링
 //		list.stream().filter(each -> each instanceof Meeting)
@@ -45,9 +43,15 @@ public class CalendarAppApplication {
 //		list.stream()
 //				.filter(each -> each.support(EventType.MEETING)) // Meeting 타입 필터한 결과
 //				.forEach(Event::print); // MEETING에 대한 결과만 print
+//		schedule.printBy(EventType.TO_DO);
 
 
+		// 데이터 csv 파일로 만들어 대량으로 등록하기
+		EventCsvReader csvReader = new EventCsvReader();
+		String meetingCsvPath = "/data/meeting.csv";
+		List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+		meetings.forEach(schedule::add);
 
-		schedule.printBy(EventType.TO_DO);
+		schedule.printAll();
 	}
 }
---
+++ MiniProjects/CalendarAppApplication/src/main/java/reader/EventCsvReader.java
@@ -0,0 +1,61 @@
+package reader;
+
+import com.opencsv.CSVReader;
+import event.Meeting;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class EventCsvReader {
+    public List<Meeting> readMeetings(String path) throws IOException { // csv 경로
+        List<Meeting> result = new ArrayList<>();
+
+        // 데이터 읽는 부분
+        // List는 String 배열을 가지고 있는 데이터
+        List<String[]> read = readAll(path);
+
+        for (int i=0; i<read.size(); i++) {
+            if (skipHeader(i)) continue; // header skip
+
+            String[] each = read.get(i);
+
+            // Meeting으로 변환 부분
+            result.add(
+                    new Meeting(Integer.parseInt(each[0]),
+                            each[2],
+                            ZonedDateTime.of(LocalDateTime.parse(each[6], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), ZoneId.of("Asia/Seoul")),
+                            ZonedDateTime.of(LocalDateTime.parse(each[7], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), ZoneId.of("Asia/Seoul")),
+                            new HashSet<>(Arrays.asList(each[3].split(","))),
+                            each[4],
+                            each[5]
+                            )
+            );
+        }
+
+        // Meeting으로 변환하는 부분
+
+        return result;
+    }
+
+    private static boolean skipHeader(int i) {
+        return i == 0;
+        // method로 분리 (option + command + m)
+    }
+
+    private List<String[]> readAll(String path) throws IOException {
+        InputStream in = getClass().getResourceAsStream(path);
+        InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+        CSVReader csvReader = new CSVReader(reader);
+        return csvReader.readAll(); // 이 경로로 입력된 데이터에서 csv파일로 변환이 된 다음, 결과값을 리스트 형식으로 반환
+    }
+}
