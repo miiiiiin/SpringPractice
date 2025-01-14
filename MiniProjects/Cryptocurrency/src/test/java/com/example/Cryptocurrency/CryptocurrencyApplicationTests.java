a2a779ba3b16bc93306053e5fe4bef433b531d6f
songkyung <min.songkyung@gmail.com>
2025-01-14 17:02:08 +0900

    init: init project

---
+++ MiniProjects/Cryptocurrency/src/test/java/com/example/Cryptocurrency/CryptocurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cryptocurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
